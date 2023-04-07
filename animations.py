3caa65e0eb80d2115a49a29f1a665211dba203b2
Martix <66840932+MartixInTheMatrix@users.noreply.github.com>
2023-04-07 23:05:04 +0200

    Add files via upload

---
+++ animations.py
@@ -0,0 +1,47 @@
+import pygame, threading
+
+def set_interval(func, sec):
+   def func_wrapper():
+       set_interval(func, sec) 
+       func()  
+   t = threading.Timer(sec, func_wrapper)
+   t.start()
+   return t
+
+class PlayerAnimations:
+
+    def __init__(self, player):
+        self.STATUS = {
+            "idle": 0,
+            "walkf": 1,
+            "walkb": 2,
+            "walkr": 3,
+            "walkl": 4
+        }
+        self.player = player
+        self.ticks = [0, self.STATUS["idle"]]
+
+
+    def changeTicks(self, status):
+        if self.ticks[1] != self.STATUS[status]:
+            self.ticks = [0, self.STATUS[status]]
+
+    def animationPhases(self, path, duration): 
+        numberOfPhases = len(duration) 
+        self.ticks[0] += 1
+        for d in range(numberOfPhases):
+            if d == numberOfPhases-1 and duration[d] <= self.ticks[0]//25:
+                self.ticks[0] = 0
+                self.player.render(path + str(0) + ".png")
+                return 
+            if self.ticks[0]//25 < duration[d]:
+                self.player.render(path + str(d) + ".png")
+                return
+
+    def idle(self):
+        self.changeTicks("idle")
+        self.animationPhases("./resources/animations/player/idle/idle0", [4, 5, 9, 10])
+
+    def walk(self, direction):
+        self.changeTicks("walk"+direction)
+        self.animationPhases("./resources/animations/player/walk/walk"+direction, [0.5, 1.5, 2])
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pygame, random
-import time, threading
-from perlin_noise import PerlinNoise
+import pygame, random, time, threading
+from animations import PlayerAnimations
 
 StartTime=time.time()
 MAP_SIZE = 6400
@@ -8,91 +7,93 @@ SCREEN_WIDTH=1280
 SCREEN_HEIGHT=720
 
 COLORS = {
-    0: (0, 0, 255), # Water
-    1: (0, 255, 0), # Plains
-    2: (160, 160, 160) # Mountains
+    0: (0, 255, 0),
+    1: (0, 0, 255),
+    2: (160, 160, 160)
 }
-
-def log(*args, **kw): # Debug
-    print(*args, **kw)
 
 class Tilemap:
     def __init__(self):
         self.map = []
         self.generateMap()
 
-    def generateMap(self): # fonction a modifier c'est pour tester les couleurs
-        self.map = [[0 for j in range(MAP_SIZE)] for i in range(MAP_SIZE)]
-        noise = PerlinNoise(octaves = 50, seed = 500)
+    def generateMap(self): # fonction a modifier c'est pour tester les couleurs 
+        # plains
+        self.map = [[0 for i in range(MAP_SIZE)] for j in range(MAP_SIZE)]
         
-        log("Generating Terrain")
-        
-        # Generating Terrain
-        for i in range(1000):
-            for j in range(1000):
-                height = abs(noise((i / MAP_SIZE, j / MAP_SIZE))) * 255
-                if height < 10:
-                    self.map[i][j] = 0
-                elif height < 60:
-                    self.map[i][j] = 1
-                else:
-                    self.map[i][j] = 2
+        # mountains
+        for i in range(MAP_SIZE):
+            for j in range(200):
+                self.map[i][j] = random.randint(0, 2)
 
-        log("Terrain Generated.")
 
 
     def render(self, surface, player_x, player_y):
         # 32x32
-        for x, c in enumerate(self.map[player_x: player_x + 40]): # bouge en fonction du joueur. 40 = 1280 / 32, 23 = 720 / 32
-            for y, tile in enumerate(c[player_y: player_y + 23]):
+        for x, c in enumerate(self.map[player_x:player_x+40]): # bouge en fonction du joueur
+            for y, tile in enumerate(c[player_y:player_y+23]):
                 pygame.draw.rect(surface, COLORS[tile], pygame.Rect(x * 32, y * 32, 32, 32))
 
-class FPSCounter:
-    def __init__(self, clock, screen, player):
+class FPS_counter:
+    def __init__(self, clock, screen):
         self.clock = clock
         self.screen = screen
-        self.player = player
 
     def display(self):
         font = pygame.font.SysFont(None, 30)
-        img = font.render(f"{round(self.clock.get_fps())}, X: {self.player.x}, Y: {self.player.y}", True, (0, 0, 0))
+        img = font.render(str(round(self.clock.get_fps())), True, (0, 0, 0))
         self.screen.blit(img, (2, 2))
 
 class Player:
-    def __init__(self, x, y):
+    def __init__(self, x, y, screen):
         self.x = x
         self.y = y
         self.speed = 1
+        self.screen = screen
 
-    def render(self, screen):
-        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, 32, 32)) # joueur toujours au millieu de l'écran, c'est le bg qui bouge
+    def render(self, skin="./resources/animations/player/idle/idle00.png"):
+        img = pygame.transform.scale(pygame.image.load(skin), (128, 128))
+        self.screen.blit(img, (SCREEN_WIDTH//2, SCREEN_HEIGHT//2)) # joueur toujours au millieu de l'écran, c'est le bg qui bouge
 
 class EventHandler:
-    def __init__(self, game):
-        self.game = game # Game pointer
+    def __init__(self):
         self.cooldown = 0 # pour ralentir la vitesse du personnage, l'action d'avancer ne s'execute qu'une fois sur 2
 
     def didQuit(self):
         # Did the user click the window close button?
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game.running = False
-                    
-    def movePlayer(self, player):
-        keys = pygame.key.get_pressed()
+                if event.type == pygame.QUIT:
+                    running = False
+    def ralentir(self):
         self.cooldown += 1
-        if self.cooldown >= 2:
+        if self.cooldown == 2:
             self.cooldown = 0
-            return
-        if keys[pygame.K_z] and player.y-1 > 20: # verif pour pas que le personnage ne sorte de la map
-            player.y -= player.speed 
-        if keys[pygame.K_q] and player.x-1 > 12:
-            player.x -= player.speed
-        if keys[pygame.K_s] and player.y+1 < MAP_SIZE-20:
-            player.y += player.speed
-        if keys[pygame.K_d] and player.x+1 < MAP_SIZE-12:
-            player.x += player.speed
+            return False
+        return True
+
+    def movePlayer(self, player, playerAnimations):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_z]: # verif pour pas que le personnage ne sorte de la map
+            playerAnimations.walk("b")
+            if player.y-player.speed > 20 and self.ralentir():
+                player.y -= player.speed 
+        if keys[pygame.K_q]:
+            if player.x-player.speed > 12 and self.ralentir(): 
+                player.x -= player.speed
+        if keys[pygame.K_s]:
+            playerAnimations.walk("f")
+            if player.y+player.speed < MAP_SIZE-20 and self.ralentir():
+                player.y += player.speed
+        if keys[pygame.K_d]:
+            if player.x+player.speed < MAP_SIZE-12 and self.ralentir():
+                player.x += player.speed
         
+        if not (keys[pygame.K_z] or keys[pygame.K_q] or keys[pygame.K_s] or keys[pygame.K_d]):
+            playerAnimations.idle()
+
+            
+            
 class Game:
     def __init__(self):
         # Initialize pygame
@@ -100,31 +101,28 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0)
         self.tilemap = Tilemap()
         self.clock = pygame.time.Clock()
-        self.player = Player(10, 10)
-        self.fps_counter = FPSCounter(self.clock, self.screen, self.player)
-        self.event_handler = EventHandler(self)
-        self.running = True
-        
+        self.fps_counter = FPS_counter(self.clock, self.screen)
+        self.player = Player(10, 10, self.screen)
+        self.playerAnimations = PlayerAnimations(self.player)
+        self.EventHandler = EventHandler()
+
     def run(self):
         # Run until the user asks to quit
-        
+        running = True
 
-        while self.running:
-
-            self.event_handler.didQuit()
-            self.event_handler.movePlayer(self.player)
-
+        while running:
+            self.EventHandler.didQuit()
+            time.sleep(0.01)
             # Fill the background with white
             self.screen.fill((255, 255, 255))
 
             # Draw
             self.tilemap.render(self.screen, self.player.x, self.player.y)
-            self.player.render(self.screen)
 
             # fps
             self.clock.tick()
             self.fps_counter.display()
-
+            self.EventHandler.movePlayer(self.player, self.playerAnimations)
             # Flip the display
             pygame.display.flip()
 
